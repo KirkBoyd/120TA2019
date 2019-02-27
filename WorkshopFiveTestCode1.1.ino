d5a1ddd116b09a44e03234aa35388f87f001c301
Kirk Boyd <40477730+KirkBoyd@users.noreply.github.com>
2019-02-26 19:25:31 -0500

    Add files via upload

---
+++ WorkshopFiveTestCode1.1.ino
@@ -0,0 +1,34 @@
+ // Connect a start switch/touch sensor to digital port 12
+// Use analog port 1 for Sharp sensor
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address and config
+// Global variables
+int Sharp1 = 3; //use analog port 3 for Sharp sensor
+int start_sw = 12; //digital port ID, start switch
+int interval = 250; //interval between sensor readings
+void start_press(int sw){
+  //sw represents the digital port number that’s connected to the switch
+  //function waits for switch to be pressed and released
+  while(digitalRead(sw) == HIGH){} //exit when switch pressed
+  while(digitalRead(sw) == LOW) {} // continue when switch released
+}
+void setup(){
+  // initialize the display
+  lcd.init();
+  lcd.backlight();
+  //clear the LCD and prompt the user
+  lcd.clear();
+  lcd.print("Hit Start Button");
+  start_press(start_sw);
+  pinMode(Sharp1, INPUT);
+}
+void loop(){
+  int Reading;
+  Reading = analogRead(Sharp1);
+  //clear the LCD and print the sensor reading
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(Reading,DEC);
+  delay(interval);
+}
---
+++ wkshp4servosTA.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>;
+#include <Wire.h>;
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+Servo myServo;
+int servoport = 9;
+int pos = 0;
+void servoTest(int val){
+    lcd.clear();
+    lcd.print("pos");
+    lcd.print(val, DEC);
+    myServo.write(val);
+    delay(50);
+}
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  pinMode(servoport, OUTPUT);
+  myServo.attach(servoport);
+  
+}
+
+void loop() {
+  servoTest(90);
+  for(int i=90; i<181;i++){
+    servoTest(i);
+    delay(1);
+  }
+  for(int i=180; i>-1;i--){
+    servoTest(i);
+    delay(1);
+  }
+  for(int i=0; i<91;i++){
+    servoTest(i);
+    delay(1);
+  }
+  delay(1000000000000);
+//  int delta = 50;
+//  for(pos=0; pos>181;pos++){
+//    lcd.clear();
+//    lcd.print("pos");
+//    lcd.print(pos, DEC);
+//    myServo.write(pos);
+//    delay(delta);
+//  }
+//  for(pos=180; pos>0;pos--){
+//    lcd.clear();
+//    lcd.print("pos");
+//    lcd.print(pos, DEC);
+//    myServo.write(pos);
+//    delay(delta);
+//  }
+}
+
+
